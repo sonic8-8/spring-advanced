913634a4acbeb203a20e9ea7c3e63393ed72239f
이승환 <seaung13@gmail.com>
2025-03-05 07:00:22 +0900

    test: 템플릿 메서드 패턴 - 추상 클래스 적용 예제 추가

--- src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
+++ src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -1,5 +1,7 @@
 package hello.advanced.trace.template;
 
+import hello.advanced.trace.template.code.SubClassLogic1;
+import hello.advanced.trace.template.code.SubClassLogic2;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -14,7 +16,9 @@ public class TemplateMethodTest {
 
     private void logic1() {
         long startTime = System.currentTimeMillis();
+        // 비즈니스 로직 실행
         log.info("비즈니스 로직1 실행");
+        // 비즈니스 로직 종료
         long endTime = System.currentTimeMillis();
         long resultTime = endTime - startTime;
         log.info("resultTime={}", resultTime);
@@ -22,9 +26,23 @@ public class TemplateMethodTest {
 
     private void logic2() {
         long startTime = System.currentTimeMillis();
+        // 비즈니스 로직 실행
         log.info("비즈니스 로직2 실행");
+        // 비즈니스 로직 종료
         long endTime = System.currentTimeMillis();
         long resultTime = endTime - startTime;
         log.info("resultTime={}", resultTime);
+    }
+
+    /**
+     * 템플릿 메서드 패턴 적용
+     */
+    @Test
+    void templateMethodV1() {
+        SubClassLogic1 template1 = new SubClassLogic1();
+        template1.execute();
+
+        SubClassLogic2 template2 = new SubClassLogic2();
+        template2.execute();
     }
 }
